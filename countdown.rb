61d3c145a02ff6004fdf8389fc798b8a39f2f22e
Amal Abdu <abduamal@ymail.com>
2020-03-05 20:32:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,15 @@
 #write your code here
 
 def countdown(number)
-  while number
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+end
+
+puts "HAPPY NEW YEAR!"
+
+def countdown_with_sleep(count)
+  
+  
 end
